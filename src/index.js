c2ae822acdb1efa44ee563d83d08d632ac166d20
raha mohebbi <raha.mohebbi.03@gmail.com>
2025-04-15 13:46:35 +0330

    add store and provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
+import Store from './app/Store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter> 
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
+<React.StrictMode>
+  <BrowserRouter>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+</React.StrictMode>
 );
