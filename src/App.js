296b50cb99408af593de6c50c0779df985ccc51c
raha mohebbi <raha.mohebbi.03@gmail.com>
2025-04-15 13:27:04 +0330

    ad toastify and routes and theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Movie from './components/Movie';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
+import CartPage from './components/CartPage';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
-  return (
-    <Fragment>
-<Navbar/>
-    <Movie/>
-    </Fragment>
-  );
+return (
+  <Fragment>
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Movie />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+
+
+
+    </ThemeProvider>
+  </Fragment>
+);
 };
 
 export default App;
