14565618349134eafc77cdffb74810d272c50277
raha mohebbi <raha.mohebbi.03@gmail.com>
2025-04-15 13:40:18 +0330

    we have a loder in this file which first i write it in pragraph then i changed it to classnames

---
+++ src/components/loader.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Loading() {
+return (
+    <div className="flex justify-center items-center h-96">
+        <div className="w-14 h-14 border-4 border-purple-500 border-dashed rounded-full animate-spin"></div>
+    </div>
+);
+}
+
+export default Loading;
